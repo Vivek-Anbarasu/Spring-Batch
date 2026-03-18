2fd9d4795007aaeca2c7d4ad20700a2539da40f1
Vivek <vivek.sixdee@gmail.com>
2026-03-18 15:23:09 +0530

    Updated to Java 25

--- src/main/java/com/springbatch/config/BatchConfig.java
+++ src/main/java/com/springbatch/config/BatchConfig.java
@@ -1,12 +1,9 @@
 package com.springbatch.config;
 
-import javax.sql.DataSource;
-
 import org.springframework.batch.core.job.Job;
 import org.springframework.batch.core.step.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.infrastructure.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,16 +24,7 @@ public class BatchConfig {
     @Autowired
     MariaDBBatch mariaDBBatch;
 
-    @Bean
-    public JobRepository jobRepository(
-            @Qualifier("postgresDataSource") DataSource dataSource,
-            @Qualifier("batchTransactionManager") PlatformTransactionManager transactionManager) throws Exception {
-        JobRepositoryFactoryBean factory = new JobRepositoryFactoryBean();
-        factory.setDataSource(dataSource);
-        factory.setTransactionManager(transactionManager);
-        factory.afterPropertiesSet();
-        return factory.getObject();
-    }
+
 
     @Bean
     public Job postGresJob(JobRepository jobRepository, @Qualifier("postGresStep") Step postGresStep) {
